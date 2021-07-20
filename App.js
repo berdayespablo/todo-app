988e5b5a86a44a485dd5c2a71f6018a89ec60169
BerdyDesktop <berdayespablo@gmail.com>
2021-07-20 15:21:27 +0200

    Basic Maquetation

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header'
+import InputBar from './components/InputBar'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor (){
+    super();
+  
+    this.state = {
+      todoInput: '',
+      todos: [
+        { id: 0, title: 'Take out the trash', done: false},
+        {id: 1, title: 'Cook dinner', done: false}
+      ]
+    }
+  }
+
+  render(){
+    const statusbar = (Platform.OS == 'web') ? <View style={styles.statusbar}></View> : <View></View>
+
+    return (
+      <View style={styles.container}>
+         {statusbar}
+
+         <Header title="TODO APP"/>
+
+        <InputBar textChange = {todoInput => this.setState({todoInput})}/>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  statusbar: {
+    backgroundColor: '#FFCE00',
+    height: 20
+  }
 });
