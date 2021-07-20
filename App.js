d5c1833b8f65f112216f739858be6c5388a353d5
BerdyDesktop <berdayespablo@gmail.com>
2021-07-20 18:13:55 +0200

    Aplicacion Cliente v1

--- App.js
+++ App.js
@@ -1,35 +1,87 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header'
-import InputBar from './components/InputBar'
+import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
+import InputBar from './components/InputBar';
+import TodoItem from './components/TodoItem';
 
 export default class App extends React.Component {
-  constructor (){
+  constructor () {
     super();
-  
+
     this.state = {
       todoInput: '',
       todos: [
-        { id: 0, title: 'Take out the trash', done: false},
-        {id: 1, title: 'Cook dinner', done: false}
+        { id: 0, title: 'Take out the trash', done: false },
+        { id: 1, title: 'Cook dinner', done: false }
       ]
     }
   }
 
-  render(){
-    const statusbar = (Platform.OS == 'web') ? <View style={styles.statusbar}></View> : <View></View>
+  addNewTodo () {
+    let todos = this.state.todos;
+
+    todos.unshift({
+      id: todos.length + 1,
+      title: this.state.todoInput,
+      done: false
+    });
+
+    this.setState({
+      todos: todos,
+      todoInput: ''
+    });
+  }
+
+  toggleDone (item) {
+    let todos = this.state.todos;
+
+    todos = todos.map((todo) => {
+      if (todo.id == item.id) {
+        todo.done = !todo.done;
+      }
+
+      return todo;
+    })
+
+    this.setState({todos});
+  }
+
+  removeTodo (item) {
+    let todos = this.state.todos;
+
+    todos = todos.filter((todo) => todo.id !== item.id);
+
+    this.setState({todos});
+  }
+
+  render() {
+    const statusbar = (Platform.OS == 'web') ? <View style={styles.statusbar}></View> : <View></View>;
 
     return (
       <View style={styles.container}>
-         {statusbar}
+        {statusbar}
 
-         <Header title="TODO APP"/>
+        <Header title="Todoapp" />
 
-        <InputBar textChange = {todoInput => this.setState({todoInput})}/>
+        <InputBar
+          addNewTodo={() => this.addNewTodo()}
+          textChange={todoInput => this.setState({ todoInput })}
+          todoInput={this.state.todoInput}
+        />
+
+        <FlatList
+          data={this.state.todos}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <TodoItem todoItem={item} toggleDone={() => this.toggleDone(item)} removeTodo={() => this.removeTodo(item)} />
+            )
+          }}
+        />
       </View>
     );
   }
-  
 }
 
 const styles = StyleSheet.create({
